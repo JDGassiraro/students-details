f0ade16ea762b12caf8555a4719453cf6ef59954
JDGassiraro <josephgassiraro2021@gmail.com>
2024-09-22 15:52:10 -0500

    moving assests into the public folder and adding a search bar by using ng bootstrap and angular form modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,22 @@
+
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {FormsModule, FormControl, FormBuilder, ReactiveFormsModule} from '@angular/forms';
+import {NgbHighlight} from '@ng-bootstrap/ng-bootstrap'
+
+
 
 @Component({
     selector: 'students-details-root',
     standalone: true,
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, CommonModule, FormsModule, ReactiveFormsModule, NgbHighlight],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
 export class AppComponent {
     title = 'students-details';
+    filter: FormControl;
     students: Array<any> =[
     {
         "name": "Marcus Meepers",
@@ -114,4 +120,11 @@ export class AppComponent {
         
        
     ];
+    constructor(
+        private fb: FormBuilder
+
+    ) {
+        this.filter= this.fb.control ("",{nonNullable : true});
+    }
 }
+
